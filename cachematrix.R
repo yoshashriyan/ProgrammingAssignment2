5fcdc4a85b42fda04cd18a641de122a0e806f12b
Yosha Udaya Shriyan <yosha.shriyan@gmail.com>
2020-08-06 16:15:12 +0530

    Functions for chaching the inverse of a matrix created

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,33 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  mi <- NULL
+  set <- function(y){
+    x <<- y
+    mi <<- NULL
+  }
+  
+  get <- function() x
+  setinverse <- function(inverse) mi <- inverse
+  getinverse <- function() mi
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse) #return obj
+  
 }
+
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  mi <- x$getinverse()
+  if(!is.null(mi)){
+    message("getting chache data")
+    return(mi)
+  }
+  
+  data <- x$get()
+  mi <- solve(data, ...)
+  x$setinverse(mi)
+  mi
 }
